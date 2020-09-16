c74867db3ddf1dbd39e24fbcf9954a7267e6da67
manataDesign <manata.design07@gmail.com>
2020-09-17 08:33:36 +0900

    ユアコーディング

---
+++ js/mainScript.js
@@ -0,0 +1,48 @@
+$(function () {
+    //アコーディオンメニュー
+    $('#acMenu dt').on('click', function () {
+        $(this).next().slideToggle(300);
+        // return false;
+    });
+
+    //FVを超えたらheaderが固定する
+    $(window).on('load scroll',function (){
+        var fvHeight = $('.fv').outerHeight(),
+            value = $(this).scrollTop();
+        if(value > fvHeight){
+            $('.header').addClass('fixed');
+        }else {
+            $('.header').removeClass('fixed');
+        }
+    });
+
+    //hamburgerを押したらnavが全画面表示される
+    $('.hamburger').on('click',function (){
+        $(this).toggleClass('close');
+        $('.header-nav').toggleClass('open');
+        $('body').toggleClass('noscroll');
+        //#でページ上部に行くのを防ぐ
+        return false;
+    });
+
+    //nav(sm)のリンクを押した時
+    $('.header-nav__item > a').on('click',function (){
+        $('.header-nav').removeClass('open');
+        $('.hamburger').removeClass('close');
+        $('body').removeClass('noscroll');
+    });
+
+    //checkboxを押さない限り送信ができない
+    $('#submit').on('click',function (){
+        if($('.click').length) {
+            return true;
+        }else {
+            alert('プライバシーポリシーに同意してください');
+            return false;
+        }
+    });
+
+    $('#privacy').on('click',function (){
+        $('#submit').toggleClass('click');
+    });
+});
